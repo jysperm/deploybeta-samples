027a0b04a756eed6c4e7c44eeaa99197aad764f4
jysperm <jysperm@gmail.com>
2018-08-04 22:48:27 +0800

    nodejs-redis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Redis = require('ioredis');
+
+var redis = new Redis('redis://' + process.env['DATA_SOURCE_REDIS'])
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  redis.info().then( info => {
+    res.type('text').send(info);
+  }).catch( err => {
+    res.status(500).type('text').send(err)
+  });
 });
 
 module.exports = router;
